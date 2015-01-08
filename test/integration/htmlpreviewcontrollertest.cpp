9238ae6233aaecad7e30a685356156acb0b6e7a5
Christian Loose <christian.loose@hamburg.de>
2015-01-08 16:56:02 +0100

    [MVC] Move zoom actions and context menu to HtmlPreviewController
    Move setup of zoom actions and custom context menu to the
    HtmlPreviewController class.

--- test/integration/htmlpreviewcontrollertest.cpp
+++ test/integration/htmlpreviewcontrollertest.cpp
@@ -16,6 +16,7 @@
  */
 #include "htmlpreviewcontrollertest.h"
 
+#include <QApplication>
 #include <QtTest>
 #include <QWebView>
 
@@ -25,6 +26,9 @@ void HtmlPreviewControllerTest::initTestCase()
 {
     webView = new QWebView();
     controller = new HtmlPreviewController(webView);
+
+    webView->show();
+    qApp->setActiveWindow(webView);
 }
 
 void HtmlPreviewControllerTest::cleanupTestCase()
@@ -63,4 +67,32 @@ void HtmlPreviewControllerTest::resetsZoomFactorOnZoomReset()
     QCOMPARE(webView->zoomFactor(), 1.0);
 }
 
+void HtmlPreviewControllerTest::zoomsInOnCtrlPlusKeyPress()
+{
+    qreal previousZoomFactor = 1.0;
+    webView->setZoomFactor(previousZoomFactor);
 
+    QTest::keyClick(webView, Qt::Key_Plus, Qt::ControlModifier);
+
+    QVERIFY(webView->zoomFactor() > previousZoomFactor);
+}
+
+void HtmlPreviewControllerTest::zoomsOutOnCtrlMinusKeyPress()
+{
+    qreal previousZoomFactor = 1.0;
+    webView->setZoomFactor(previousZoomFactor);
+
+    QTest::keyClick(webView, Qt::Key_Minus, Qt::ControlModifier);
+
+    QVERIFY(webView->zoomFactor() < previousZoomFactor);
+}
+
+void HtmlPreviewControllerTest::resetsZoomOnCtrlZeroKeyPress()
+{
+    qreal previousZoomFactor = 2.0;
+    webView->setZoomFactor(previousZoomFactor);
+
+    QTest::keyClick(webView, Qt::Key_0, Qt::ControlModifier);
+
+    QCOMPARE(webView->zoomFactor(), 1.0);
+}
